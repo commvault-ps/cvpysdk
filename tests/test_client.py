a96fc84c1a88e5b230b78bf7bcc06eeda0b4a640
James Tan <jamestan@commvault.com>
2026-02-22 19:32:33 +0800

    fix(tests): fix silent assertNotEqual bug in test_file_upload

--- tests/test_client.py
+++ tests/test_client.py
@@ -28,16 +28,10 @@ class ClientTest(testlib.SDKTestCase):
         super(ClientTest, self).tearDown()
 
     def test_client_init(self):
-        self.assertRaises(
-            SDKException,
-            self.commcell_object.clients.get,
-            'abc123')
-        self.assertIsInstance(
-            self.commcell_object.clients.get(
-                self.client_name), Client)
+        self.assertRaises(SDKException, self.commcell_object.clients.get, "abc123")
+        self.assertIsInstance(self.commcell_object.clients.get(self.client_name), Client)
 
     def test_backup_activity(self):
-
         if self.client.is_backup_enabled:
             self.assertIsNone(self.client.disable_backup())
             self.assertFalse(self.client.is_backup_enabled)
@@ -46,7 +40,6 @@ class ClientTest(testlib.SDKTestCase):
             self.assertTrue(self.client.is_backup_enabled)
 
     def test_restore_activity(self):
-
         if self.client.is_restore_enabled:
             self.assertIsNone(self.client.disable_restore())
             self.assertFalse(self.client.is_restore_enabled)
@@ -55,7 +48,6 @@ class ClientTest(testlib.SDKTestCase):
             self.assertTrue(self.client.is_restore_enabled)
 
     def test_data_aging(self):
-
         if self.client.is_data_aging_enabled:
             self.assertIsNone(self.client.disable_data_aging())
             self.assertFalse(self.client.is_data_aging_enabled)
@@ -64,29 +56,33 @@ class ClientTest(testlib.SDKTestCase):
             self.assertTrue(self.client.is_data_aging_enabled)
 
     def test_execute_command(self):
-        """ Execute  mkdir command with wrong input folder path. Check if output matches
-            regex specified
+        """Execute  mkdir command with wrong input folder path. Check if output matches
+        regex specified
         """
         import re
-        self.assertRegexpMatches(self.client.execute_command("mkdir !:")[1],
-                                 re.compile("\w*cannot find\w*"))
+
+        self.assertRegexpMatches(
+            self.client.execute_command("mkdir !:")[1], re.compile("\w*cannot find\w*")
+        )
 
     def test_readiness(self):
         self.assertTrue(self.client.is_ready)
 
     def test_file_upload(self):
         import os
+
         # testing for CS client , hence assuming windows.
         destination_folder = "C:\\"
         destination_file = destination_folder + os.path.basename(__file__)
         self.client.execute_command("del " + destination_file)
         self.client.upload_file(__file__, destination_folder)
-        assertNotEqual(self.client.execute_command(
-            "if exists " + destination_file + " echo success"),
-            "success")
+        self.assertNotEqual(
+            self.client.execute_command("if exists " + destination_file + " echo success"),
+            "success",
+        )
 
 
 if __name__ == "__main__":
-
     import unittest
+
     unittest.main()
