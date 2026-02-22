7562bfa9da97875ea003ecbf7cb28d289e40c163
James Tan <jamestan@commvault.com>
2026-02-22 19:34:17 +0800

    refactor(tests): rename test_json to private method and remove dead imports

--- tests/testlib.py
+++ tests/testlib.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
 
 # --------------------------------------------------------------------------
 # Copyright  Commvault Systems, Inc.
@@ -8,50 +7,47 @@
 # --------------------------------------------------------------------------
 
 """Shared unit test utilities."""
+
 import json
-import os
-import time
 
 try:
     import unittest2 as unittest
 except ImportError:
     import unittest
 
-from cvpysdk import commcell
-from time import sleep
-from datetime import datetime, timedelta
-
 import logging
+
+from cvpysdk import commcell
+
 logging.basicConfig(
-    filename='unit_test.log',
-    level=logging.DEBUG,
-    format="%(asctime)s:%(levelname)s:%(message)s")
+    filename="unit_test.log", level=logging.DEBUG, format="%(asctime)s:%(levelname)s:%(message)s"
+)
 
 
 class SDKTestCase(unittest.TestCase):
-
     @classmethod
     def setUpClass(cls):
-        with open('input.json', 'r') as data_file:
+        with open("input.json") as data_file:
             cls.data = json.load(data_file)
 
     def setUp(self):
         unittest.TestCase.setUp(self)
-        self.test_json()
-        self.commcell_object = commcell.Commcell(**self.data['commcell'])
+        self._validate_json_config()
+        self.commcell_object = commcell.Commcell(**self.data["commcell"])
 
     def tearDown(self):
         self.commcell_object.logout()
 
-    def test_json(self):
+    def _validate_json_config(self):
         self.assertIsInstance(self.data, dict)
-        self.assertIsInstance(self.data['commcell'], dict)
-        self.assertIn('webconsole_hostname', self.data['commcell'].keys())
-        self.assertIn('commcell_username', self.data['commcell'].keys())
-        self.assertIn('commcell_password', self.data['commcell'].keys())
-        self.assertNotIn("", self.data['commcell'].values())
+        self.assertIsInstance(self.data["commcell"], dict)
+        self.assertIn("webconsole_hostname", self.data["commcell"].keys())
+        self.assertIn("commcell_username", self.data["commcell"].keys())
+        self.assertIn("commcell_password", self.data["commcell"].keys())
+        self.assertNotIn("", self.data["commcell"].values())
 
 
 if __name__ == "__main__":
     import unittest
+
     unittest.main()
